5d276b816ba97336d30b38bfa88987980baf11ef
kunalsinghpatel <kunalsingh7053patel@gmail.com>
2026-02-24 23:20:34 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold text-center text-blue-600'>Hostel Hub</h1>
+    </div>
+  )
+}
+
+export default App
